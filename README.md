c6753774a19a84f61bea9d4cf7029293a04e577e
OctoBirb <53840213+OctoBirb@users.noreply.github.com>
2022-05-16 22:02:32 -0400

    a0.0.1_z
    Final before release of a0.0.2

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 
 ## Alpha phase
 a0.0.1 - added the barebones minimum of what's considered an incremental game
+<br>
 a0.0.1_1 - added comments and also added a button for generator a1 (units-1)
+<br>
 a0.0.1_2 - nothing happened, i forgot to commit this version so it doesn't exist, like the 9th dimension
+<br>
 a0.0.1_3 - a bit more css
+<br>
 a0.0.1_4 - some more buttons - i'm 1/3 of the way to 0.02 (also text on the buttons)
+<br>
+a0.0.1_5 to a0.0.1_f - a lot of refresh rate changing, until i realized that my school computer was bad
+<br>
+a0.0.1_x - a lot more html, but not enough js
+a0.0.1_y - a lot more css
+
 **a0.0.2** - all 9 generators are complete
--- script.js
+++ script.js
@@ -14,7 +14,42 @@ function getUnit(x) {
 
 var UG1 = {
     cost: 10,
-    bups: 1,
+    bups: 1.1,
+    mult: 1,
+    have: 0
+}
+
+var UG2 = {
+    cost: 100,
+    bups: 12.1,
+    mult: 1,
+    have: 0
+}
+
+var KG1 = {
+    cost: 11,
+    bups: 1.2,
+    mult: 1,
+    have: 0
+}
+
+var KG2 = {
+    cost: 121,
+    bups: 14.4,
+    mult: 1,
+    have: 0
+}
+
+var MG1 = {
+    cost: 12,
+    bups: 1.3,
+    mult: 1,
+    have: 0
+}
+
+var MG2 = {
+    cost: 144,
+    bups: 16.9,
     mult: 1,
     have: 0
 }
@@ -26,6 +61,56 @@ function buyUG1() {
         units.u -= UG1.cost
         UG1.have += 1
         UG1.cost *= costscaling
+    } else {
+        console.log("bruh");
+    }
+}
+
+function buyUG2() {
+    if (units.u >= UG2.cost) {
+        units.u -= UG2.cost
+        UG2.have += 1
+        UG2.cost *= costscaling
+    } else {
+        console.log("bruh");
+    }
+}
+
+function buyKG1() {
+    if (units.u >= KG1.cost) {
+        units.u -= KG1.cost
+        KG1.have += 1
+        KG1.cost *= costscaling
+    } else {
+        console.log("bruh");
+    }
+}
+
+function buyKG2() {
+    if (units.u >= UG2.cost) {
+        units.u -= KG2.cost
+        KG2.have += 1
+        KG2.cost *= costscaling
+    } else {
+        console.log("bruh");
+    }
+}
+
+function buyMG1() {
+    if (units.u >= MG1.cost) {
+        units.u -= MG1.cost
+        MG1.have += 1
+        MG1.cost *= costscaling
+    } else {
+        console.log("bruh");
+    }
+}
+
+function buyMG2() {
+    if (units.u >= MG2.cost) {
+        units.u -= MG2.cost
+        MG2.have += 1
+        MG2.cost *= costscaling
     } else {
         console.log("bruh");
     }
